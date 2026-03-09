7b232168bb886d4056e1c9d695cd098e4d00e801
Achyut-shekhar <achyutshekhar54@gmail.com>
2026-03-09 10:43:01 +0530

    Add Number Sorter functionality with HTML and JavaScript
    - Created a new HTML file for a number sorting application.
    - Implemented input for adding numbers and displaying them in sorted order.
    - Added functionality to clear the list of numbers.
    - Included basic styling for better user experience.

---
+++ prac/1.js
@@ -0,0 +1,10 @@
+let str = "achyut shekhar";
+let arr = str.split(" ");
+console.log(arr);
+
+let arr1 = str.slice(0, 2);
+console.log(arr1);
+
+let newarr = ["achyut", "shekhar"];
+let arr2 = newarr.splice(0, 1);
+console.log(arr2);
